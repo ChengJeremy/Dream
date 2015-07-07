47833a60b0ed3374b622119fc46787f5bb7329b1
Jeremy Cheng <jyz.cheng@gmail.com>
2015-07-07 18:12:25 -0400

    Dream
    Renamed Everything

---
+++ src/dream/gfx/ImageLoader.java
@@ -0,0 +1,23 @@
+package dream.gfx;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
+public class ImageLoader {
+    
+    public static BufferedImage loadImage(String path){
+        try {
+            //this line is all that matters:
+            return ImageIO.read(ImageLoader.class.getResource(path));
+            
+        } catch (IOException ex) {
+            Logger.getLogger(ImageLoader.class.getName()).log(Level.SEVERE, null, ex);
+            System.exit(1);
+        }
+        return null;
+    }
+    
+}
