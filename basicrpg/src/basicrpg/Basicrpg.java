ccb9d6656b7eec110f0f2a26dac53389aac4d7d8
Jeremy Cheng <jyz.cheng@gmail.com>
2015-07-07 18:06:50 -0400

    Revert "Revert "basicrpg""
    This reverts commit 8a3cc83c2b2527f9b688899352b468855e4f23b0.

---
+++ basicrpg/src/basicrpg/Basicrpg.java
@@ -0,0 +1,13 @@
+package basicrpg;
+
+public class Basicrpg {
+
+    public static void main(String[] args) {
+        
+        //new Display("Dream", 300, 300);
+        Game game = new Game("Dream", 400, 400);
+        game.start();
+        
+    }
+    
+}
