d41316c290cfabe986d00e1759767696a0a2dd32
Jeremy Cheng <jyz.cheng@gmail.com>
2015-07-22 17:14:52 -0400

    added new player
    a hooded figure wanders mysteriously within the deep caverns of the
    mind... lost... and seemingly without purpose (SPOILER ALERT: HE IS
    BEING CHASED BY A DEMON OF THE PAST :O

--- src/dream/gfx/Assets.java
+++ src/dream/gfx/Assets.java
@@ -4,17 +4,17 @@ import java.awt.image.BufferedImage;
 
 public class Assets {
     
-    private static final int width = 35, height = 31;
+    private static final int width = 90, height = 114;
     
     public static BufferedImage player, dirt, water, tree;
     
     public static void init(){
         SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("/textures/tileset.png"));
         
-        player = sheet.crop(11, 40, width, height);
-        tree = sheet.crop(width, 0, width, height);
-        water = sheet.crop(width * 2, 0, width, height);
-        dirt = sheet.crop(0, height, width, height);
+        player = sheet.crop(20, 0, width, height);
+        //tree = sheet.crop(width, 0, width, height);
+        //water = sheet.crop(width * 2, 0, width, height);
+        //dirt = sheet.crop(0, height, width, height);
         
     }
     
